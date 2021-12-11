4779bed84101ebc57ab8f92f42eeddcac1373a2f
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-11 11:12:36 +0700

    init commit

---
+++ app/src/main/java/com/dicoding/myservice/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.dicoding.myservice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStartService: Button = findViewById(R.id.btn_start_service)
+        btnStartService.setOnClickListener{
+            val mStartServiceIntent = Intent(this, MyService::class.java)
+            startService(mStartServiceIntent)
+        }
+
+        val btnStartJobIntentService: Button = findViewById(R.id.btn_start_job_intent_service)
+        btnStartJobIntentService.setOnClickListener{
+
+        }
+
+        val btnStartBoundService: Button = findViewById(R.id.btn_start_bound_service)
+        btnStartBoundService.setOnClickListener {
+
+        }
+
+        val btnStopBoundService: Button = findViewById(R.id.btn_stop_bound_service)
+        btnStopBoundService.setOnClickListener {
+
+        }
+    }
+}
